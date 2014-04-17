e40d5fcc757ab963bf4c39abe07ec9676a7222e3
Martin Czygan <martin.czygan@gmail.com>
2014-04-17 21:23:24 +0200

    add -n option to normalize ISBNs

--- isbngrep.go
+++ isbngrep.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "bufio"
+    "bytes"
     "fmt"
     "github.com/codegangsta/cli"
     "io"
@@ -54,6 +55,33 @@ func is_valid_isbn_13(s string) (bool, string) {
     return check == string(s[12]), s
 }
 
+// assume isbn is in 10 format already
+func isbn_10_to_13(s string) string {
+    var buffer bytes.Buffer
+    buffer.WriteString("978")
+    buffer.WriteString(s[:9])
+
+    // s contains the ISBN13 w/o checksum
+    s = buffer.String()
+    // checksum calculation
+    sum := 0
+    var factor int
+    buf := make([]byte, 1)
+    for i, c := range s[:12] {
+        if i%2 == 0 {
+            factor = 1
+        } else {
+            factor = 3
+        }
+        _ = utf8.EncodeRune(buf, c)
+        value, _ := strconv.Atoi(string(buf))
+        sum += value * factor
+    }
+    check := strconv.Itoa((10 - (sum % 10)) % 10)
+    buffer.WriteString(check)
+    return buffer.String()
+}
+
 func main() {
     app := cli.NewApp()
     app.Flags = []cli.Flag{
@@ -61,6 +89,7 @@ func main() {
         cli.BoolFlag{"uniq, u", "return a uniq list"},
         cli.BoolFlag{"only-10, 0", "only ISBN10"},
         cli.BoolFlag{"only-13, 3", "only ISBN13"},
+        cli.BoolFlag{"normalize, n", "normalize to ISBN13"},
     }
     app.Name = "isbngrep"
     app.Usage = "find ISBNs in texts"
@@ -84,6 +113,9 @@ func main() {
                 if !c.Bool("only-13") {
                     ok, value := is_valid_isbn_10(occ)
                     if ok {
+                        if c.Bool("normalize") {
+                            value = isbn_10_to_13(value)
+                        }
                         fmt.Println(value)
                         seen[occ] = true
                         matches += 1
